49962d8f7317213878b7b13602f36583bd4d42e5
abc <abc@example.com>
2023-08-25 19:34:04 +0100

    I have updated the boundry of the least and most amount of charecters that a user should put into the generator

--- script.js
+++ script.js
@@ -99,12 +99,12 @@ if(isNaN(length) === true){
   alert('Password length must be provided as a number')
 }
 
-if(length >= 10) {
-  alert('Password length must be at least 10 charecters')
+if(length >= 8) {
+  alert('Password length must be at least 8 charecters')
 }
  
-if(length <= 65){
-  alert('password length must be less than or equal to 64 charecters =')
+if(length <= 128){
+  alert('password length must be less than or equal to 128 charecters =')
 }
 
 let hasSpecialCharecters = confirm('Click Ok to confim if you want your password to include special charecters')
